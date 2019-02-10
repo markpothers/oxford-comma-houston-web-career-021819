81d309452423221ba22ed820cb2bb0ec31acb694
Mark Pothecary <markpothers@hotmail.com>
2019-02-10 22:08:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,13 @@
 def oxford_comma(array)
+joined = []
   if array.size == 2
     array.insert(-1, "and #{array[array.size-1]}")
     array.delete_at(array.size-2)
+    joined = array.join
   elsif array.size > 2
       array.insert(-1, "and #{array[array.size-1]}")
       array.delete_at(array.size-2)
+      joined = array.join", "
     end
-  return array.join" "
+  return joined.join
 end
