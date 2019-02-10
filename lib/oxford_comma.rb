197616538e85c94350bd3fff268f3547aba18d11
Mark Pothecary <markpothers@hotmail.com>
2019-02-10 22:10:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
-  if array.size == 2
+  if array.size < 2
+    return array
+  elsif array.size == 2
     array.insert(-1, "and #{array[array.size-1]}")
     array.delete_at(array.size-2)
     return array.join(" ")
